bddd2a889062222eb9a2a97e5bcb0df9f17a93c9
SirRamEsq <gmrl95@gmail.com>
2021-06-13 16:56:01 -0400

    Merge old configs

--- .scripts/newLinuxInstall/basicSoftwareInstall.sh
+++ .scripts/newLinuxInstall/basicSoftwareInstall.sh
@@ -5,7 +5,7 @@ cd ~
 
 # Basic software install
 sudo apt update
-sudo apt install git curl zsh sakura tmux neovim ranger xfonts-utils flameshot nmap fortune cowsay python3 python3-pip curl wget dunst i3blocks ncmpcpp compton dmenu feh gsimplecal unclutter filezilla unrar
+sudo apt install git curl zsh sakura tmux neovim ranger xfonts-utils flameshot nmap fortune cowsay python3 python3-pip curl wget dunst i3blocks ncmpcpp compton dmenu feh gsimplecal unclutter filezilla unrar ncmpcpp pcmanfm
 sudo pip3 install neovim
 sudo pip3 install pywal
 
@@ -45,3 +45,6 @@ sudo apt install audacity
 
 # Discord
 sudo snap install discord
+
+# Run Wal once
+wal -i ~/.config/wall.jpeg
--- .scripts/newLinuxInstall/setup-i3-gaps.sh
+++ .scripts/newLinuxInstall/setup-i3-gaps.sh
@@ -2,12 +2,15 @@
 
 ###
 ### For Debian systems
+### See here: https://lottalinuxlinks.com/how-to-build-and-install-i3-gaps-on-debian/
 ###
 
-# Install dependencies for i3-gaps
-sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf xutils-dev libtool automake meson
+# Install dependencies for i3 (IMPORTANT)
+sudo apt install i3
 
-sudo apt install dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev
+# Install dependencies for i3-gaps
+
+sudo apt install meson dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev
 
 # Move to home
 cd ~
@@ -29,5 +32,5 @@ git checkout gaps && git pull
 rm -rf ./build
 mkdir ./build
 cd build
-meson ..
-ninja
+meson --prefix /usr/local
+sudo ninja install
