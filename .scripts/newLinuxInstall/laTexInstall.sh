9865b2cd01e8571a5a242d58b56cfca585b31502
SirRamEsq <gmrl95@gmail.com>
2021-07-12 09:37:29 -0400

    Add latex install script

---
+++ .scripts/newLinuxInstall/laTexInstall.sh
@@ -0,0 +1,7 @@
+# Install latex
+sudo apt install texlive-base texlive-bibtex-extra
+sudo apt install texlive-xetex
+
+# fonts
+sudo apt install texlive-fonts-recommended
+sudo apt install texlive-fonts-extra
